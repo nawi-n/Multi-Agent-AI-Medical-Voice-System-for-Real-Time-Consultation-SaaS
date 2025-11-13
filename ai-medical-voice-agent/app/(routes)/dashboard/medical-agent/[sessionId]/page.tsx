8276718efb3c7600bbc9a2e2368e851d0b201f16
NAWIN R <nawincpn@gmail.com>
2025-11-13 11:01:36 +0530

    Integrate Vapi for voice call functionality....02:30
    Added Vapi integration to enable starting and ending voice calls in the medical agent dashboard. The UI now reflects call status, displays live transcripts, and shows recent messages. Updated dependencies to include @vapi-ai/web.

--- ai-medical-voice-agent/app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ ai-medical-voice-agent/app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -4,9 +4,10 @@ import { useParams } from "next/navigation";
 import React, { use, useEffect, useState } from "react";
 import Image from "next/image";
 import { DoctorAgent } from "../../_components/DoctorAgentCard";
-import { Circle, PhoneCall } from "lucide-react";
+import { Circle, PhoneCall, PhoneOff, Slice } from "lucide-react";
 import { eq } from "drizzle-orm";
 import { Button } from "@/components/ui/button";
+import Vapi from "@vapi-ai/web";
 
 type sessionDetail = {
   id: number;
@@ -17,9 +18,19 @@ type sessionDetail = {
   createdOn: string;
 };
 
+type message = {
+  role: string;
+  text: string;
+};
+
 function MedicalVoiceAgent() {
   const { sessionId } = useParams();
   const [sessionDetail, setSessionDetail] = useState<sessionDetail>();
+  const [callStarted, setCallStarted] = useState(false);
+  const [vapiInstance, setVapiInstance] = useState<any>();
+  const [currentRole, setCurrentRole] = useState<string | null>();
+  const [liveTranscript, setLiveTranscript] = useState<string>();
+  const [messages, setMessages] = useState<message[]>([]);
 
   useEffect(() => {
     sessionId && GetSessionDetails();
@@ -30,11 +41,67 @@ function MedicalVoiceAgent() {
     setSessionDetail(result.data);
   };
 
+  const StartCall = () => {
+    const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY || "");
+    setVapiInstance(vapi);
+    vapi.start(process.env.NEXT_PUBLIC_VAPI_VOICE_ASSISTANT_ID || "");
+    vapi.on("call-start", () => {
+      console.log("Call started");
+      setCallStarted(true);
+    });
+    vapi.on("call-end", () => {
+      console.log("Call ended");
+      setCallStarted(false);
+    });
+    vapi.on("message", (message) => {
+      if (message.type === "transcript") {
+        const { role, transcriptType, transcript } = message;
+        console.log(`${message.role}: ${message.transcript}`);
+        if (transcriptType === "partial") {
+          setLiveTranscript(transcript);
+          setCurrentRole(role);
+        } else if (transcriptType === "final") {
+          setMessages((prevMessages: any) => [
+            ...prevMessages,
+            { role: role, text: transcript },
+          ]);
+          setLiveTranscript("");
+          setCurrentRole(null);
+        }
+      }
+    });
+    vapiInstance.on("speech-start", () => {
+      console.log("Assistant started speaking");
+      setCurrentRole("assistant");
+    });
+    vapiInstance.on("speech-end", () => {
+      console.log("Assistant stopped speaking");
+      setCurrentRole("user");
+    });
+  };
+
+  const endCall = () => {
+    if (!vapiInstance) return;
+    vapiInstance.stop();
+    vapiInstance.off("call-start");
+    vapiInstance.off("call-end");
+    vapiInstance.off("message");
+
+    setCallStarted(false);
+    setVapiInstance(null);
+  };
+
   return (
     <div className="p-5 border rounded-3xl bg-secondary">
       <div className="flex justify-between items-center">
         <h2 className="p-1 px-2 border rounded-md flex gap-2 items-center">
-          <Circle className="h-4 w-4" /> Not Connected{" "}
+          <Circle
+            className={
+              "h-4 w-4 rounded-full" +
+              (callStarted ? " bg-green-500" : " bg-red-500")
+            }
+          />{" "}
+          {callStarted ? "Connected..." : "Not Connected"}
         </h2>
         <h2 className="font-bold text-xl text-gray-400">00:00</h2>
       </div>
@@ -53,14 +120,28 @@ function MedicalVoiceAgent() {
           </h2>
           <p className="text-sm text-gray-400">AI Medical Voice Agent</p>
 
-          <div className="mt-32">
-            <h2 className="text-gray-400">Assistant Msg</h2>
-            <h2 className="text-lg">User Msg</h2>
+          <div className="mt-12 overflow-y-auto flex flex-col items-center px-10 md:28 lg:px-28 xl:px-72 ">
+            {messages?.slice(-4).map((msg: message, index) => (
+              <h2 className="text-gray-400 p-2" key={index}>
+                {msg.role}: {msg.text} Msg
+              </h2>
+            ))}
+            {liveTranscript && liveTranscript?.length > 0 && (
+              <h2 className="text-lg">
+                {currentRole} {liveTranscript}
+              </h2>
+            )}
           </div>
 
-          <Button className="mt-20">
-            <PhoneCall /> Start Call{" "}
-          </Button>
+          {!callStarted ? (
+            <Button className="mt-20" onClick={StartCall}>
+              <PhoneCall /> Start Call
+            </Button>
+          ) : (
+            <Button variant={"destructive"} onClick={endCall}>
+              <PhoneOff /> Disconnect
+            </Button>
+          )}
         </div>
       )}
     </div>
